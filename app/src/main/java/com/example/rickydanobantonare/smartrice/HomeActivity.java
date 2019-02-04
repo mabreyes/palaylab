0d0071b2b3b0f0f08ecfc59c58f5e1d439adf03f
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-02-04 17:08:31 +0800

    Splash screen is working now

--- app/src/main/java/com/example/rickydanobantonare/smartrice/HomeActivity.java
+++ app/src/main/java/com/example/rickydanobantonare/smartrice/HomeActivity.java
@@ -6,21 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT=4000;
+    private static int SPLASH_TIME_OUT = 4000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(homeIntent);
-                finish();
+                Intent mainIntent = new Intent(HomeActivity.this, MainActivity.class);
+                HomeActivity.this.startActivity(mainIntent);
+                HomeActivity.this.finish();
             }
-        },SPLASH_TIME_OUT);
+        }, SPLASH_TIME_OUT);
     }
 }
--- app/src/main/java/com/example/rickydanobantonare/smartrice/Results.java
+++ app/src/main/java/com/example/rickydanobantonare/smartrice/Results.java
@@ -5,8 +5,6 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.SimpleAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
