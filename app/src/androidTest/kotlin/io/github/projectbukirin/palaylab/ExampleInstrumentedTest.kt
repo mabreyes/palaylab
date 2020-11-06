915e6e70fdb8a45e626102adfe0ff98fbc4a9cdd
Marc Anthony Reyes <marc.reyes@mynt.xyz>
2020-11-06 23:30:18 +0800

    Refactor java to kotlin for sample instrumentation test

---
+++ app/src/androidTest/kotlin/io/github/projectbukirin/palaylab/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package io.github.projectbukirin.palaylab
+
+import android.content.Context
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext: Context = InstrumentationRegistry.getTargetContext()
+        Assert.assertEquals("io.github.projectbukirin.palaylab", appContext.packageName)
+    }
+}
